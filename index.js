c6fe17cd889d0ca11f0be8d0db64f374863df441
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-04 11:00:05 -0600

    Main express index

---
+++ index.js
@@ -0,0 +1,27 @@
+//Import express
+import express from "express";
+
+//Define express app
+const app = express();
+
+//Import routes from main routers
+import router from "./routes/index.js";
+
+// Middleware for JSON parsing
+app.use(express.json());
+
+// //Basic route at root URL
+// app.get('/', (req, res) => {
+//     res.send("Hello World!")
+// })
+
+//Use routes from main routes
+//??? Do you only have to include the root handler for all other
+//routes from main routes??? 
+app.use('/', router)
+// app.use('/about', router)
+
+//Start the server
+app.listen(3000, () => {
+    console.log("Express app listening on port 3000")
+})
